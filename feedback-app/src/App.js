801f5ddb05f54e78cb61a3be3d95e76b46945028
unoriginal1234 <eric.t.roth@gmail.com>
2023-08-04 08:12:26 -0700

    added cards

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackData from './data/FeedbackData'
+
 function App() {
+    const [feedback, setFeedback] = useState
+    (FeedbackData)
 
     return (
-        <div className='container'>
-            <h1>My App</h1>                   
-        </div>
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackList feedback={feedback} />             
+            </div>
+        </>
     )
 }
 
