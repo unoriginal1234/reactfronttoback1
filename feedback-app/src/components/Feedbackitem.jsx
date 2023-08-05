95173b3be370a5907de56d7eeb432309bb3b2404
unoriginal1234 <eric.t.roth@gmail.com>
2023-08-04 21:20:38 -0700

    added data and components

---
+++ feedback-app/src/components/Feedbackitem.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function Feedbackitem({item}) {
+  return (
+    <Card>
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">{item.text}</div>
+    </Card>
+  )
+}
+
+Feedbackitem.propTypes = {
+    item: PropTypes.object.isRequired,
+}
+
+export default Feedbackitem
